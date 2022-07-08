577393a419ffd7838eab848d379365cb1fa54544
Abdullah Shakir <ab.shakir@zyrous.com>
2022-07-08 15:14:50 +0500

    textfield component added.

--- source/03-components/button/button.stories.jsx
+++ source/03-components/button/button.stories.jsx
@@ -4,55 +4,107 @@ import twigTemplate from './button.twig';
 import data from './button.yml';
 
 const settings = {
-  title: 'Components/Button',
+  title: 'Components/Button'
 };
 
-const Primary = args => (
-  parse(twigTemplate({
-    ...args,
-  }))
-);
+const Primary = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      modifier_classes: 'button--primary',
+    })
+  );
 Primary.args = { ...data };
 
-const Secondary = args => (
-  parse(twigTemplate({
-    ...args,
-    modifier_classes: 'c-button--secondary',
-  }))
-);
+const Secondary = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      modifier_classes: 'button--secondary',
+    })
+  );
 Secondary.args = { ...data };
 
-const Base = args => (
-  parse(twigTemplate({
-    ...args,
-    modifier_classes: 'c-button--base',
-  }))
-);
-Base.args = { ...data };
+const Tertiary = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      modifier_classes: 'button--tertiary',
+    })
+  );
+Tertiary.args = { ...data };
 
-const Danger = args => (
-  parse(twigTemplate({
-    ...args,
-    modifier_classes: 'c-button--danger',
-  }))
-);
-Danger.args = { ...data };
-
-const Small = args => (
-  parse(twigTemplate({
-    ...args,
-    modifier_classes: 'c-button--small',
-  }))
-);
+const Small = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      modifier_classes: 'button--primary button--small',
+    })
+  );
 Small.args = { ...data };
 
-const Large = args => (
-  parse(twigTemplate({
-    ...args,
-    modifier_classes: 'c-button--large',
-  }))
-);
-Large.args = { ...data };
+const ButtonWithIcon = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      modifier_classes: 'button--primary',
+    })
+  );
+ButtonWithIcon.args = { ...data, is_icon: true, text: 'Text' };
+
+const ButtonWithIconRight = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      modifier_classes: 'button--secondary',
+    })
+  );
+ButtonWithIconRight.args = { ...data, is_icon: true, text: 'Text', icon_position: 'right' };
+
+const ButtonWithIconRightAndLeft = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      modifier_classes: 'button--secondary',
+    })
+  );
+ButtonWithIconRightAndLeft.args = { ...data, is_icon: true, text: 'Text', icon_position: 'both' };
+
+const ButtonWithIconOnly = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      modifier_classes: 'button--primary',
+    })
+  );
+ButtonWithIconOnly.args = { ...data, is_icon: true, text: 'Text', icon_only: true };
+
+const SmallButtonWithIcon = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      modifier_classes: 'button--primary button--small',
+    })
+  );
+SmallButtonWithIcon.args = { ...data, is_icon: true, text: 'Text' };
+
+const Pills = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      modifier_classes: 'button--primary',
+    })
+  );
+Pills.args = { ...data, is_pill: true, text: 'Default' };
+
+const PillsWithIcon = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      modifier_classes: 'button--secondary',
+    })
+  );
+PillsWithIcon.args = { ...data, is_pill: true, is_icon: true, icon_name: 'close', text: 'Available' };
 
 export default settings;
-export { Primary, Secondary, Base, Danger, Large, Small };
+export { Primary, Secondary, Tertiary, Small, ButtonWithIcon, ButtonWithIconRight, ButtonWithIconRightAndLeft, ButtonWithIconOnly, SmallButtonWithIcon, Pills, PillsWithIcon };
